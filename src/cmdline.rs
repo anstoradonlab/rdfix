eff9210c9a32ccfd2396621bb9a72586666af02a
Alan Griffiths <algriff@gmail.com>
2023-07-24 14:33:47 +1000

    Change to struct for subcommand types

--- src/cmdline.rs
+++ src/cmdline.rs
@@ -1,5 +1,5 @@
 use anyhow::{anyhow, Result};
-use clap::{Parser, Subcommand};
+use clap::{Args, Parser, Subcommand};
 use std::{env, fs, path::PathBuf};
 
 /// Deconvolve two-filter dual-flow-loop radon detector output
@@ -23,79 +23,81 @@ fn validate_output_dir(p: &PathBuf) -> Result<()> {
 }
 
 #[derive(Subcommand, Debug)]
-enum Commands {
+pub enum Commands {
     /// Creates a template input and configuration file
-    Template {
-        /// Where to create the template
-        #[arg(short, long, value_name = "DIR", default_value=get_default_dir().join("deconv-input").into_os_string())]
-        template_dir: PathBuf,
-    },
+    Template(TemplateArgs),
     /// Run deconvolution
-    Deconv {
-        /// Sets the config file
-        #[arg(short, long, value_name = "FILE")]
-        config: PathBuf,
+    Deconv(DeconvArgs),
+}
 
-        /// Sets the output directory
-        #[arg(short, long, value_name = "OUTPUT_DIR", default_value=get_default_dir().join("deconv-output").into_os_string())]
-        output: PathBuf,
+#[derive(Args, Debug)]
+pub struct DeconvArgs {
+    /// Sets the config file
+    #[arg(short, long, value_name = "FILE")]
+    config: PathBuf,
 
-        /// Input files
-        input_files: Vec<PathBuf>,
-    },
+    /// Sets the output directory
+    #[arg(short, long, value_name = "OUTPUT_DIR", default_value=get_default_dir().join("deconv-output").into_os_string())]
+    output: PathBuf,
+
+    /// Input files
+    input_files: Vec<PathBuf>,
+}
+
+#[derive(Args, Debug)]
+pub struct TemplateArgs {
+    /// Where to create the template
+    #[arg(short, long, value_name = "DIR", default_value=get_default_dir().join("deconv-input").into_os_string())]
+    template_dir: PathBuf,
 }
 
 pub fn parse_cmdline() -> Result<RdfixArgs> {
     let args = RdfixArgs::parse_from(wild::args());
 
     match &args.command {
-        Some(Commands::Template { template_dir }) => {
-            if template_dir.exists() {
-                if !template_dir.is_dir() {
+        Some(Commands::Template(args)) => {
+            if args.template_dir.exists() {
+                if !args.template_dir.is_dir() {
                     return Err(anyhow!(
                         "Template directory path \"{0}\" is not a directory",
-                        template_dir.display()
+                        args.template_dir.display()
                     ));
                 }
-                let is_empty = template_dir.read_dir()?.next().is_none();
+                let is_empty = args.template_dir.read_dir()?.next().is_none();
                 if !is_empty {
                     return Err(anyhow!(
                         "Template directory \"{0}\" is not empty",
-                        template_dir.display()
+                        args.template_dir.display()
                     ));
                 }
             } else {
-                fs::create_dir(template_dir)?;
+                fs::create_dir(&args.template_dir)?;
             }
         }
 
-        Some(Commands::Deconv {
-            config,
-            output,
-            input_files,
-        }) => {
-            for fname in input_files {
+        Some(Commands::Deconv(args)) => {
+            for fname in args.input_files.iter() {
                 if !fname.exists() {
                     return Err(anyhow!("Input file \"{0}\" not found", fname.display()));
                 }
-                if !config.exists() {
+                if !args.config.exists() {
                     return Err(anyhow!(
                         "Configuration file \"{0}\" not found",
-                        config.display()
+                        args.config.display()
                     ));
                 }
-                if output.exists() {
-                    if !output.is_dir() {
+                if args.output.exists() {
+                    if !args.output.is_dir() {
                         return Err(anyhow!(
                             "Output directory path \"{0}\" is not a directory",
-                            output.display()
+                            args.output.display()
                         ));
                     }
-                    let is_empty = output.read_dir()?.next().is_none();
+                    let is_empty = args.output.read_dir()?.next().is_none();
                     if !is_empty {
                         return Err(anyhow!(
                             "Output directory \"{0}\" is not empty",
-                            output.display()
+                            args.output.display()
                         ));
                     }
                 }
